e76f123806984eb7a00b606b859f5d2bf2b63f86
Shiva1531 <105410394+Shiva1531@users.noreply.github.com>
2023-07-25 11:32:32 +0530

    Update home-delivery-details.component.spec.ts

--- home-delivery-details.component.spec.ts
+++ home-delivery-details.component.spec.ts
@@ -18,4 +18,10 @@ describe('HomeDeliveryDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+   it('should set correct image sources', () => {
+    fixture.detectChanges();
+    const imageElements = fixture.nativeElement.querySelectorAll('.shipment-image img');
+    expect(imageElements[0].src).toContain('assets/images/sofa1.jpg');
+    expect(imageElements[1].src).toContain('assets/images/sofa2.jpg');
+  });
 });
