cd745cf86b3f9771e45ca4df1c4d891c04e95af7
Shiva1531 <105410394+Shiva1531@users.noreply.github.com>
2023-07-25 11:37:43 +0530

    Update order-summary.component.spec.ts

--- order-summary.component.spec.ts
+++ order-summary.component.spec.ts
@@ -18,4 +18,21 @@ describe('OrderSummaryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+   it('should display the correct content for each order item', () => {
+    fixture.detectChanges();
+    const orderItems = fixture.nativeElement.querySelectorAll('.order-item');
+    expect(orderItems.length).toBe(7);
+
+    expect(orderItems[0].textContent).toContain('Subtotal for 2 items');
+    expect(orderItems[1].textContent).toContain('Protection Plans');
+    expect(orderItems[2].textContent).toContain('Furniture');
+    expect(orderItems[3].textContent).toContain('Delivery');
+    expect(orderItems[4].textContent).toContain('State Recycling Fee');
+    expect(orderItems[5].textContent).toContain('Taxes');
+});
+it('should apply "bolder" class to the "Estimated Total" amount', () => {
+  fixture.detectChanges();
+  const estimatedTotalElement = fixture.nativeElement.querySelector('.bolder');
+  expect(estimatedTotalElement).toBeTruthy();
+});
 });
