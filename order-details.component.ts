474075d693426c12d7953237dae63d61424d2e97
Shiva1531 <105410394+Shiva1531@users.noreply.github.com>
2023-07-25 11:35:31 +0530

    Update order-details.component.ts

--- order-details.component.ts
+++ order-details.component.ts
@@ -1,27 +1,27 @@
-import { Component,OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order-details',
   templateUrl: './order-details.component.html',
   styleUrls: ['./order-details.component.css']
 })
-export class OrderDetailsComponent  implements OnInit{
-  orderForm!: FormGroup;
+export class OrderDetailsComponent implements OnInit {
+  myForm!: FormGroup;
   showForm: boolean = false; // Variable to control the form visibility
 
-  constructor(private formBuilder: FormBuilder) {}
-  
-  ngOnInit(): void {
-    this.orderForm = this.formBuilder.group({
-      orderDate: [''],
-      orderNumber: [''],
-      paymentMethod: [''],
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      orderDate: ['', [Validators.required]],
+      orderNumber: ['', [Validators.required, Validators.maxLength(8)]],
+      paymentMethod: ['', [Validators.required]],
       billingAddress: ['']
     });
   }
+
+  ngOnInit(): void {}
+
   toggleForm() {
     this.showForm = !this.showForm; // Toggle the form visibility
   }
-
 }
